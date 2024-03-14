a590b27f28760ab79c9fccbc7a7ac47f84c78183
Dylan Tettarasar <dtettarasar@gmail.com>
2024-03-14 17:01:47 +0000

    load product data in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,27 @@
 import { createStore } from 'vuex'
+import axios from 'axios'; 
 
 export default createStore({
   state: {
-    
+    products : []
   },
   mutations: {
     
+    loadProducts(state, products) {
+      state.products = products;
+    }
+
   },
   actions: {
+
+    loadProducts({commit}) {
+      axios
+      .get('https://fakestoreapi.com/products')
+      .then(response => {
+        //console.log(response.data);
+        commit('loadProducts', response.data);
+      })
+    }
 
   },
   modules: {
